b2cd8bed18b437313b9dd544e155ae71e3a39265
Evaldo Maciel <evaldomaciel17@gmail.com>
2023-01-12 18:04:59 -0300

    init app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ByteBankApp());
+}
+
+class ByteBankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Dashboard")),
+      ),
+    );
+  }
+}
